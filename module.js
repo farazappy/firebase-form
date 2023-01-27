f448559c17b73364fade5249d820e8694eda43bb
farazappy <farazappy@gmail.com>
2023-01-27 22:32:08 +0530

    Added npm scope.

--- module.js
+++ module.js
@@ -3,7 +3,7 @@ const { resolve, join } = require('path')
 export default function() {
   this.addPlugin({
     src: resolve(__dirname, 'plugins/index.js'),
-    fileName: join(namespace, 'plugins/index.js'),
+    fileName: 'plugins/index.js',
   })
 }
 module.exports.meta = require('./package.json')
